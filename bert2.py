4810acc5e4c071631ebdefbcc389971aeae78f5a
phj6688 <peyman.h.j@gmail.com>
2023-04-11 18:56:59 +0200

    fix datasets and bert model

--- bert2.py
+++ bert2.py
@@ -5,8 +5,11 @@ import shutil
 from sklearn.preprocessing import LabelEncoder
 from scipy.special import softmax
 from sklearn.model_selection import train_test_split
-from transformers import BertTokenizer, BertForSequenceClassification, Trainer, TrainingArguments
+#from transformers import BertTokenizer, BertForSequenceClassification, Trainer, TrainingArguments
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments
 from sklearn.metrics import precision_recall_fscore_support, accuracy_score, roc_auc_score
+import torch
+from torch.utils.data import Dataset
 
 #set seed to 100 
 np.random.seed(100)
@@ -16,39 +19,37 @@ np.random.seed(100)
 # disable wandb
 os.environ["WANDB_DISABLED"] = "true"
 
-class Dataset:
+class Dataset(Dataset):
     def __init__(self, encodings, labels):
         self.encodings = encodings
         self.labels = labels
 
     def __getitem__(self, idx):
         item = {key: val[idx] for key, val in self.encodings.items()}
-        item['labels'] = self.labels[idx]
+        item['labels'] = torch.tensor(self.labels[idx])
         return item
 
     def __len__(self):
         return len(self.labels)
     
 class BERT:
-    def __init__(self,train_path,test_path,trainings_arguments:TrainingArguments, model_name='bert-base-uncased'):
+    def __init__(self,train_path,test_path,trainings_arguments:TrainingArguments, model_name='distilbert-base-uncased'):
 #        self.max_seq_len = max_seq_len
         self.model_name = model_name
-        self.tokenizer = BertTokenizer.from_pretrained(self.model_name)
+        self.n_runs = None
+        self.best_val_loss = None
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
         self.trainings_arguments = trainings_arguments        
         self.train_dataset, self.test_dataset, self.n_classes = self.prepare_dataset(train_path, test_path)
-        self.model = BertForSequenceClassification.from_pretrained(self.model_name, num_labels=self.n_classes)
+        self.model = AutoModelForSequenceClassification.from_pretrained(self.model_name, num_labels=self.n_classes)
         self.compute_metrics_func = self.compute_metrics
         self.trainer = Trainer(
             model=self.model,
             args=self.trainings_arguments,
             train_dataset=self.train_dataset,
             eval_dataset=self.test_dataset,
-            compute_metrics=self.compute_metrics_func)
-        
-        
-
-        
-
+            compute_metrics=self.compute_metrics_func)    
+               
     def prepare_dataset(self, train_path, test_path):
         train_data = pd.read_csv(train_path).sample(frac=1).reset_index(drop=True)
         test_data = pd.read_csv(test_path).sample(frac=1).reset_index(drop=True)
@@ -68,7 +69,6 @@ class BERT:
         train_dataset = Dataset(train_encodings, train_data['class'].tolist())
         test_dataset = Dataset(test_encodings, test_data['class'].tolist())
         return train_dataset, test_dataset, n_classes
-
 
     def compute_metrics(self,pred):
         labels = pred.label_ids
@@ -91,34 +91,47 @@ class BERT:
             "auc": auc
         }
     
-    def run_n_times(self, dataset_name, n=3):
-        res_dict = {}
-        best_val_loss = float('inf')
-        for i in range(n):
-            print(f'Run {i+1} of {n}')
-            self.trainer.train()
-            self.trainer.save_model(f"models/bert/{dataset_name}_run_{i+1}_model")
-            results = self.trainer.evaluate()
-            res_dict[i+1] = results
-            if results['eval_loss'] < best_val_loss:
-                best_val_loss = results['eval_loss']
-                self.trainer.save_model(f"models/bert/{dataset_name}_best_model")
-            self.model.init_weights()
-            # delete the model folders to save space except for the best model
-        for i in range(n):
+    def train_and_evaluate(self, run_idx, dataset_name):
+        print(f'Run {run_idx} of {self.n_runs}')
+        self.trainer.train()
+        self.trainer.save_model(f"models/bert/{dataset_name}_run_{run_idx}_model")
+        results = self.trainer.evaluate()
+        if results['eval_loss'] < self.best_val_loss:
+            self.best_val_loss = results['eval_loss']
+            self.trainer.save_model(f"models/bert/{dataset_name}_best_model")
+        self.model.init_weights()
+        return results
+
+    def clean_up_models(self, dataset_name):
+        for i in range(self.n_runs):
             shutil.rmtree(f"models/bert/{dataset_name}_run_{i+1}_model")
-            
 
+    def calculate_and_save_averages(self, res_dict, dataset_name):
         avg_dict = {metric: round(sum(values[metric] for values in res_dict.values()) / len(res_dict), 4) for metric in res_dict[1].keys()}
+        order = ['loss', 'auc', 'f1', 'accuracy']
+        filtered_metrics = {key: avg_dict[i] for i in avg_dict if i.split('_')[-1] in order}
+        os.makedirs("results/bert", exist_ok=True)
+        with open(f"results/bert/{dataset_name}_avg_results.txt", "w") as f:
+            for key, value in filtered_metrics.items():
+                f.write(f"{key}: {value}\n")
+        return filtered_metrics
 
-        # Save the average results to disk
-        # filter the metrics to only include the ones we want        
+    def run_n_times(self, dataset_name, n=3):
+        self.n_runs = n
+        self.best_val_loss = float('inf')
+        res_dict = {}
+        
+        for i in range(n):
+            res_dict[i+1] = self.train_and_evaluate(i+1, dataset_name)
+            
+        self.clean_up_models(dataset_name)
+        avg_metrics = self.calculate_and_save_averages(res_dict, dataset_name)
         new_dict = {}
         order = ['loss', 'auc', 'f1','accuracy']
-        for i in avg_dict:
+        for i in avg_metrics:
             if i.split('_')[-1] in order:
                 key = i.split('_')[-1]
-                new_dict[key] = avg_dict[i]
+                new_dict[key] = avg_metrics[i]
 
         os.makedirs("results/bert", exist_ok=True)        
         with open(f"results/bert/{dataset_name}_avg_results.txt", "w") as f:
@@ -126,5 +139,7 @@ class BERT:
                 f.write(f"{key}: {value}\n")
 
         return new_dict
+        
+
 
 
--- run_bert.py
+++ run_bert.py
@@ -1,8 +1,8 @@
-from bert import BERT
+from bert2 import BERT
 from transformers import TrainingArguments
 
-#dataset_list = ['trec','agnews', 'pc', 'yelp', 'cr', 'kaggle_med', 'cardio', 'bbc', 'sst2','subj']
-dataset_list = ['trec']
+dataset_list = ['trec','agnews', 'pc', 'yelp', 'cr', 'kaggle_med', 'cardio', 'bbc', 'sst2','subj']
+#dataset_list = ['trec']
 
 if __name__ == '__main__':
     for name in dataset_list:
@@ -10,23 +10,25 @@ if __name__ == '__main__':
             print(f'Running {name} dataset')
             train_path  = f'data/original/{name}/train.csv'
             test_path   = f'data/original/{name}/test.csv'
-            model_name = 'bert-base-uncased'
+            model_name = 'distilbert-base-uncased'
             training_args = TrainingArguments(
                 output_dir='./results/bert',
                 num_train_epochs=3,
-                per_device_train_batch_size=16,
-                per_device_eval_batch_size=64,
+                per_device_train_batch_size=8,
+                per_device_eval_batch_size=8,
                 evaluation_strategy="epoch",
-                eval_steps=100,
+                # eval_steps=100,
                 logging_steps=10,
-                save_steps=0,
+                # save_steps=0,
                 logging_dir='./logs/bert',
-                learning_rate=2e-5,
+                metric_for_best_model="f1",
+                #learning_rate=2e-5,
+                seed=100
             )
 
 
 
-            bert = BERT(train_path, test_path, training_args, model_name=model_name, max_seq_len=512)            
+            bert = BERT(train_path, test_path, training_args, model_name=model_name)            
             avg_dict = bert.run_n_times(name, n=3)
             print('---------------------------------------------------')
             print(f'Average results for {name} dataset')
