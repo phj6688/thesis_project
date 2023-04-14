c84777f51079204e1292e5bc637f231a7b63f7e5
root <root@Peyman.localdomain>
2023-04-14 18:14:59 +0200

    add simple_bert from simpletransformers

--- run_bert.py
+++ run_bert.py
@@ -1,40 +1,29 @@
-from bert2 import BERT
-from transformers import TrainingArguments
+from simple_bert import SimpleBert
+import numpy as np
 
-dataset_list = ['trec','agnews', 'pc', 'yelp', 'cr', 'kaggle_med', 'cardio', 'bbc', 'sst2','subj']
-#dataset_list = ['trec']
+np.random.seed(100)
 
-if __name__ == '__main__':
-    for name in dataset_list:
-        try:
-            print(f'Running {name} dataset')
-            train_path  = f'data/original/{name}/train.csv'
-            test_path   = f'data/original/{name}/test.csv'
-            model_name = 'distilbert-base-uncased'
-            training_args = TrainingArguments(
-                output_dir='./results/bert',
-                num_train_epochs=3,
-                per_device_train_batch_size=8,
-                per_device_eval_batch_size=8,
-                evaluation_strategy="epoch",
-                # eval_steps=100,
-                logging_steps=10,
-                # save_steps=0,
-                logging_dir='./logs/bert',
-                metric_for_best_model="f1",
-                #learning_rate=2e-5,
-                seed=100
-            )
+dataset_list = ['cr', 'trec', 'agnews', 'pc', 'yelp', 'kaggle_med', 'cardio', 'bbc', 'sst2','subj']
+#dataset_list = ['agnews']
 
 
+if __name__ == "__main__":   
+    
+    
+    #text = 'not too many great features'
+    for dataset in dataset_list:
+        print(f'Running {dataset} dataset')
+        simple_bert = SimpleBert(dataset)
+        print(f"Loaded data for {dataset} dataset")
+        simple_bert.load_data()
+        print(f"Trained model for {dataset} dataset")
+        simple_bert.train_model()
+        print(f"Evaluated model for {dataset} dataset")
+        res = simple_bert.evaluate_model()                
+        # model will be saved during the training process
+        simple_bert.save_results(f"results/bert/{dataset}_result.txt")
+        print(f"Saved model and results for {dataset} dataset")
+        print(f'results: \n\n\n{res}\n\n\n')
 
-            bert = BERT(train_path, test_path, training_args, model_name=model_name)            
-            avg_dict = bert.run_n_times(name, n=3)
-            print('---------------------------------------------------')
-            print(f'Average results for {name} dataset')
-            print(avg_dict)
-            print('---------------------------------------------------')
-        except Exception as e:
-            print(f'Error in {name}')
-            print(str(e))
-            continue
+        #pre_last_layer_output = simple_bert.extract_pre_last_layer(text)
+        #print(pre_last_layer_output)
---
+++ simple_bert.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import numpy as np
+from simpletransformers.classification import ClassificationModel
+import torch
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.preprocessing import LabelEncoder
+
+np.random.seed(100)
+
+class SimpleBert:
+    def __init__(self, dataset_name):
+        self.dataset_name = dataset_name
+        self.args = {
+        'output_dir': f'./models/bert/{self.dataset_name}',
+        'reprocess_input_data': True,
+        'overwrite_output_dir': True,
+        'train_batch_size': 8,
+        'num_train_epochs': 3,
+        'use_multiprocessing': False,
+        'use_multiprocessing_for_evaluation': False,
+        }
+        self.train = None
+        self.test = None
+        self.model = None
+        self.result = None
+        self.num_labels = None
+        self.result_metrics = None
+
+    def load_data(self):
+        encoder = LabelEncoder()
+        self.train = pd.read_csv(f'data/original/{self.dataset_name}/train.csv').sample(frac=1) # shuffle
+        self.test = pd.read_csv(f'data/original/{self.dataset_name}/test.csv')
+        self.train = self.train[['text', 'class']]
+        self.test = self.test[['text', 'class']]
+        self.train.columns = ['text', 'labels']
+        self.test.columns = ['text', 'labels']
+        self.train['labels'] = encoder.fit_transform(self.train['labels']) # encode the labels to start from 0
+        self.test['labels'] = encoder.transform(self.test['labels'])
+        self.num_labels = self.train['labels'].nunique()
+
+    def train_model(self):        
+        self.model = ClassificationModel('distilbert', 'distilbert-base-uncased', num_labels=self.num_labels, cuda_device=0, use_cuda=True, args=self.args)
+        self.model.train_model(self.train)
+
+    def compute_metrics(self, preds, labels):
+        self.result_metrics
+        acc = accuracy_score(labels, preds)
+        prec = precision_score(labels, preds, average='macro')
+        rec = recall_score(labels, preds, average='macro')
+        f1 = f1_score(labels, preds, average='macro')
+
+        return {
+            'acc': acc,
+            'f1': f1,
+            'prec': prec,
+            'rec': rec
+        }
+    
+    def evaluate_model(self):
+        self.result, _, _ = self.model.eval_model(self.test, compute_metrics=self.compute_metrics)
+        self.result_metrics = self.result
+        return self.result_metrics
+
+    # def save_model(self, output_dir):
+    #     self.model.save_model(output_dir)
+
+    def save_results(self, output_file):
+        with open(output_file, 'w') as f:
+            if 'compute_metrics' in self.result_metrics:
+                metrics = self.result_metrics['compute_metrics']
+                for key, value in metrics.items():
+                    f.write(f"{key}: {round(value,4)}\n")
+            for key, value in self.result_metrics.items():
+                if key != 'compute_metrics':
+                    f.write(f"{key}: {round(value,4)}\n")
+
+
+
+    def extract_pre_last_layer(self, text):
+        # Tokenize the input text
+        tokenizer = self.model.tokenizer
+        inputs = tokenizer(text, return_tensors="pt")
+
+        # Move input tensors to the same device as the model (CPU or GPU)
+        inputs = {k: v.to(self.model.device) for k, v in inputs.items()}
+
+        # Get the output from the base model (layer before the last layer)
+        with torch.no_grad():
+            base_model_output = self.model.model.distilbert(**inputs)
+
+        hidden_states = base_model_output.last_hidden_state
+        return hidden_states
